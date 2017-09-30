b2dc9876e44de861fb88a3638259aaffb14a9bfe
erik eriksson <error.errorsson@gmail.com>
2017-09-30 10:27:51 +0200

    swipe works

--- m/swipe.js
+++ m/swipe.js
@@ -1,53 +1,27 @@
 "use strict";
 
-/**
- * Bootstrap Carousel Swipe v1.1
- *
- * jQuery plugin to enable swipe gestures on Bootstrap 3 carousels.
- * Examples and documentation: https://github.com/maaaaark/bcSwipe
- *
- * Licensed under the MIT license.
- */
+var THRESHOLD = 50;
 
-(function($) {
-    $.fn.bcSwipe = function(settings) {
-	var config = { threshold: 50 };
-	if (settings)
-	    $.extend(config, settings);
-	
-	this.each(function() {
-	    var stillMoving = false;
-	    var start;
-	    
-	    if ('ontouchstart' in document.documentElement)
-		this.addEventListener('touchstart', onTouchStart, false);
-	    
-	    function onTouchStart(e) {
-		if (e.touches.length == 1) {
-		    start = e.touches[0].pageX;
-		    stillMoving = true;
-		    this.addEventListener('touchmove', onTouchMove, false);
-		}
-	    }
-
-	    function onTouchMove(e) {
-		if (stillMoving) {
-		    var x = e.touches[0].pageX;
-		    var difference = start - x;
-		    if (Math.abs(difference) >= config.threshold) {
-			cancelTouch();
-			$(this).carousel(difference > 0 ? 'next' : 'prev');
-		    }
-		}
-	    }
-
-	    function cancelTouch() {
-		this.removeEventListener('touchmove', onTouchMove);
-		start = null;
-		stillMoving = false;
-	    }
-	});
-
-	return this;
-    };
-})(jQuery);
+$(() => {
+    log('ontouchstart' in window ?
+        'Touch supported' : 'Touch not supported');
+    $('.carousel').each((_, carousel) => {
+        var $carousel = $(carousel)
+        $carousel.on('touchstart', e => {
+            if (!e.touches.length)
+                return;
+            var start = e.touches[0].pageX;
+            $carousel.on('touchmove', e => {
+                var x = e.touches[0].pageX;
+                var diff = start - x;
+                if (Math.abs(diff) >= THRESHOLD) {
+                    $carousel.off('touchmove');
+                    $carousel.carousel(diff > 0 ? 'next' : 'prev');
+                }
+            });
+        });
+        $carousel.on('touchcancel', e => {
+            $carousel.off('touchmove');
+        });
+    });
+});
